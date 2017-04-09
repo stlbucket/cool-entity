e009431048e107cb7bd667084b3fe6801c012949
stlbucket <stlbucket@gmail.com>
2017-04-09 15:23:29 -0500

    1.0.5

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  CoolClient: require('./src/coolClient'),
   CoolEntity: require('./src/coolEntity'),
   CoolRelation: require('./src/coolRelation'),
   CoolCollection: require('./src/coolCollection')
